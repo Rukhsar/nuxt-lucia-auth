2dc47d1d61690490328c7e7acbc5c28bc934a10b
Rukhsar <rukhsar.man@gmail.com>
2024-07-25 15:34:12 +0100

    shadcn ui added

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="flex flex-col justify-center items-center min-h-screen gap-8">
+		<h2 class="text-5xl font-bold text-gray-600">Lucia Auth</h2>
+		<div class="flex gap-4">
+			<Button variant="ghost">Login</Button>
+			<Button variant="ghost">Register</Button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup></script>
